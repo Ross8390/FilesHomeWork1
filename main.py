83a785c0094b3855ae6c4e097ea5bd39a6184d88
“Aleksandr <ross1990@mail.ru>
2022-12-03 00:09:43 +0300

    Change1

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-
+with open('data.txt', encoding='utf-8') as f:
+    cook_book = {}
+    for line in f:
+        dish_name = line.strip()
+        ingredients_count = int(f.readline())
+        ingredients = []
+        for i in range(ingredients_count):
+            ingredient = f.readline().strip()
+            ingredient_name, quantity, measure = ingredient.split(' | ')
+            ingredients.append({
+            'ingredient_name': ingredient_name,
+            'quantity': quantity,
+            'measure': measure
+            })
+        f.readline()
+        cook_book[dish_name] = ingredients
+print(cook_book)
